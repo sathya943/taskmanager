11b1c8035061fca03d71db96b06d066b0e1f2d31
sathya943 <satyasai.datascience943@gmail.com>
2026-05-13 15:43:50 +0530

    feat: add task_manager function

---
+++ task_manager.py
@@ -0,0 +1,12 @@
+# Python Taskmanager
+def task_manager():
+    tasks = []
+    while True:
+        # Display available options
+        if len(tasks) == 0:
+            print("No tasks to display.")
+        else:
+            for i, task in enumerate(tasks):
+                print(f"{i+1}. {task}")
+
+
